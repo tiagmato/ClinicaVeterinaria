1ac04eaea63fe532919533cf4db2aa6b232214a4
Toshiba L50 <Toshiba L50@TOSHIBA>
2017-04-21 17:08:46 +0100

    adição do controler Donos

---
+++ ClinicaVeterinaria/ClinicaVeterinaria/Controllers/DonosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClinicaVeterinaria.Models;
+
+namespace ClinicaVeterinaria.Controllers
+{
+    public class DonosController : Controller
+    {
+        private VetsDB db = new VetsDB();
+
+        // GET: Donos
+        public ActionResult Index()
+        {
+            return View(db.Donos.ToList().OrderBy(d=>d.Nome));
+        }
+
+        // GET: Donos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donos donos = db.Donos.Find(id);
+            if (donos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donos);
+        }
+
+        // GET: Donos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DonoID,Nome,NIF")] Donos donos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donos.Add(donos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donos);
+        }
+
+        // GET: Donos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donos donos = db.Donos.Find(id);
+            if (donos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donos);
+        }
+
+        // POST: Donos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DonoID,Nome,NIF")] Donos donos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donos);
+        }
+
+        // GET: Donos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donos donos = db.Donos.Find(id);
+            if (donos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donos);
+        }
+
+        // POST: Donos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donos donos = db.Donos.Find(id);
+            db.Donos.Remove(donos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
